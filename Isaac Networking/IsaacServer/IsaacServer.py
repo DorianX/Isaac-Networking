fefe3278a3172429e655b6cef33c02e680348b64
DorianX <dorianx@icloud.com>
2017-06-20 23:14:23 +0200

    Initial commit

---
+++ Isaac Networking/IsaacServer/IsaacServer.py
@@ -0,0 +1,193 @@
+import socket
+import select
+import json
+import re
+from parse import *
+import random
+
+# adapted functions from SolarPolarMan for seed generation.
+def getCheckSum(seed):
+    checksum = 0
+    while True:
+        checksum = (checksum + (seed & 0xFF)) & 0xFF
+        checksum = (2 * checksum + (checksum >> 7)) & 0xFF
+        seed = seed >> 5
+        if seed==0:
+            break
+    return checksum
+
+def genSeed():
+    seed = random.randint(1, 4294967295)
+    checksum = getCheckSum(seed)
+    abc = "ABCDEFGHJKLMNPQRSTWXYZ01234V6789"
+    seedsprev = ((seed ^ 0xFEF7FFD) << 8) | checksum
+    finalseed = ""
+    for loopindex in range(0, 8):
+        finalseed = abc[seedsprev & 0x1F] + finalseed
+        seedsprev = seedsprev >> 5
+    return finalseed[:4]+" "+finalseed[4:] # add the space
+
+IsaacClients = {}
+ClientState = {}
+IP_to_CID = {}
+IP_to_buffer = {}
+
+file = open("config.json", "r")
+file = file.read()
+config = json.loads(file)
+
+seed = config['seed']
+
+if seed == "GENERATE":
+    print("Generating Seed")
+    seed = genSeed()
+    print("Seed : " + seed)
+
+buffer_limit = config['bufferMax']
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, True)
+server.bind(('', config['port']))
+server.listen(config['maxClient'])
+
+num_clients = 0
+num_sim_clients = 0
+
+server_launched = True
+connected_clients = []
+
+
+def CloseConnection(client):
+    global num_sim_clients
+    num_sim_clients -= 1
+    connected_clients.remove(client)
+    del IP_to_buffer[client.getpeername()]
+    ClientState[IP_to_CID[client.getpeername()]] = "'Disconnected'"
+    client.close()
+
+
+def InitiateConnections(connections_asked):
+    global num_clients
+    global num_sim_clients
+    for connection in connections_asked:  # connect the clients
+        if num_sim_clients < config['maxClient']:
+            connection_client, info_client = connection.accept()
+            print("Client Accepted")
+
+            connected_clients.append(connection_client)
+            print("Sending Seed")
+
+            num_clients += 1
+            num_sim_clients += 1
+
+            connection_client.sendall((seed + str(num_clients) + "\n").encode())  # sends the string plus the client ID
+            IP_to_CID[info_client] = num_clients
+            IP_to_buffer[info_client] = b""
+            ClientState[num_clients] = "'Connected'"
+
+
+def ReceiveLastFrame(client):
+    continue_to_recv = True
+    last_frame = b""
+    receive_counter = 0
+    buffer_flushed = False
+    while continue_to_recv:
+        if client._closed is False:
+            try:
+                buffer_index = client.getpeername()
+                IP_to_buffer[buffer_index] += client.recv(1024)  # let's receive
+            except socket.error:
+                CloseConnection(client)
+                pass
+            else:
+                if client._closed is False:
+                    receive_counter += 1
+                    if len(IP_to_buffer[buffer_index]) > buffer_limit: # flush the buffer on size limit
+                        IP_to_buffer[buffer_index] = b""
+                    if receive_counter > 10: # if it takes too long, go take care of another client
+                        continue_to_recv = False
+                        last_frame = b"[UNFINISHED]"
+                    if b"[END]" in IP_to_buffer[buffer_index]: # if the clients want to end the connection
+                        CloseConnection(client)
+                        continue_to_recv = False
+                        last_frame = b"[END]"
+                    elif b"\n" in IP_to_buffer[buffer_index]: # on EOL : a new frame begins, and the preceding one ends
+
+                        lines = IP_to_buffer[buffer_index].split(b"\n")
+                        IP_to_buffer[buffer_index] = lines[len(lines) - 1] # place what's after the EOL in the buffer
+
+                        if buffer_flushed is False: # if the buffer wasn't flush because it was too big, the preceding frame sucessfully ended (check if
+                            last_frame = lines[len(lines) - 2]
+                            continue_to_recv = False
+        else :
+            continue_to_recv = False
+            last_frame = b"[CLOSED]"
+
+    return last_frame.decode()
+
+
+def BuildServerFrame():
+    frame = "{"  # start the generation of the JSON table that will be sent
+    for player in IsaacClients.values():
+        if ClientState[int(player['ID'])] != "'Disconnected'":
+            frame = frame \
+                    + "'" + player['ID'] \
+                    + "':{'ID':" + player['ID'] \
+                    + ", 'STATE':" + ClientState[int(player['ID'])] \
+                    + ",'PLUGINDATA':" + player['PluginData'] \
+                    + "},"
+    if frame[len(frame)-1] == ',':
+        frame = frame[:len(frame) - 1] + "}\n"
+    else:
+        frame += "}\n"
+    return frame
+
+
+def SendFrame(wlist, frame):
+    for client_to_send in wlist:
+        if (client_to_send is not server and client_to_send._closed is False):
+            try:
+                rtrn = client_to_send.sendall(frame.encode())
+            except socket.error:
+                CloseConnection(client_to_send)
+                pass
+            else:
+                print(rtrn)
+
+
+while server_launched:
+
+    connections_asked, wlist, xlist = select.select([server],
+                                                    [], [], 0.01)
+
+    InitiateConnections(connections_asked)
+
+    clients_to_read = []
+    try:
+        clients_to_read, wlist, xlist = select.select(connected_clients, connected_clients, [],
+                                                      0.01)  # see who to read from and who to send
+    except select.error:
+        pass
+    else:
+        for client in clients_to_read:
+            if client._closed is False:
+                line = ReceiveLastFrame(client)
+                if config["enableLog"] is True:
+                    print(line)
+                data = parse("[ID]{ID}[PLUGINDATA]{PluginData}", line)
+                if data is not None:
+                    IsaacClients[data['ID']] = data
+                    if int(data['ID']) in ClientState:
+                        if ClientState[int(data['ID'])] != "'Disconnected'":
+                            ClientState[int(data['ID'])] = "'Connected'"
+                    else:
+                        ClientState[int(data['ID'])] = "'Connected'"
+
+        frame_to_send = BuildServerFrame()
+        if config["enableLog"] is True:
+            print(frame_to_send)
+        SendFrame(wlist, frame_to_send)
+
+print("Server closing")
+for client in connected_clients:
+    client.close()
---
+++ Isaac Networking/main.lua
@@ -0,0 +1,229 @@
+StartDebug()
+socket = require("socket")
+json = require('json')
+
+-- Network variables (don't touch) --
+	IsaacNetwork = {}
+
+	local client
+
+	local Behaviour = {}
+	Behaviour.IDLE = 0
+	Behaviour.CLIENT = 1
+
+	local clientID = 0
+	IsaacNetwork.clientID = clientID
+
+	local connectedClients = {}
+	IsaacNetwork.connectedClients = connectedClients
+
+	local currentBehaviour = 0 -- current behaviour : client/idle
+	IsaacNetwork.currentBehaviour = currentBehaviour
+
+	IsaacNetwork.useServerSeed = true 
+
+	local connectIP = ""
+	IsaacNetwork.connectIP = connectIP
+
+	local currentPort = 21666
+
+	local baseSeed = "";
+	IsaacNetwork.baseSeed = baseSeed;
+
+	local loopTimeout = 10;
+
+	local Network = {} -- my own network API, to simplify things
+
+	local Plugins = {}
+	Plugins.Names = {}
+	Plugins.Senders = {}
+	Plugins.Processors = {}
+
+local Mod = RegisterMod("Isaac Networking", 1)
+
+local game = Game();
+
+-- Network functions (don't touch) --
+
+	local function RegisterNetworkPlugin(name, sender, processor)
+		Plugins.Names[#Plugins.Names+1]=name
+		Plugins.Senders[name]=sender
+		Plugins.Processors[name]=processor
+	end
+	IsaacNetwork.RegisterNetworkPlugin = RegisterNetworkPlugin
+
+	local function BuildPluginDataTable()
+		local DataTable = {}
+		for k,v in pairs(Plugins.Names) do
+			local sender = Plugins.Senders[v]
+			DataTable[v] = sender()
+		end
+		return DataTable
+	end
+
+	local function SendDataToPlugins()
+		local PluginDatas = {}
+		for k,v in pairs(connectedClients) do
+			PluginDatas[k] = v.PLUGINDATA
+		end
+
+			for i,NAME in pairs(Plugins.Names) do
+				local DataToGive = {}
+				for CID,DATA in pairs(PluginDatas) do
+					DataToGive[CID] = DATA[NAME]
+				end
+				local proc = Plugins.Processors[NAME]
+				proc(DataToGive)
+			end
+
+	end
+
+	local function ClientThread()
+			if currentBehaviour == Behaviour.CLIENT then
+					local player = Game():GetPlayer(0)
+					local roomindex = Game():GetLevel():GetCurrentRoomIndex()
+					local JSONString = json.encode(BuildPluginDataTable())
+					Network.SendData("[ID]"..clientID.."[PLUGINDATA]"..JSONString) -- Transmit all interesting information to the server
+					client:settimeout(0)
+					local playertable = Network.Read(client)
+					if playertable ~= nil then
+						connectedClients = json.decode(playertable)
+            			IsaacNetwork.connectedClients = connectedClients;
+						SendDataToPlugins()
+					end
+					client:settimeout(loopTimeout)
+			end
+	end
+
+	local rBuf = "";
+	function Network.Read(socket)
+			local l, err
+			local lastLine
+			l = ""
+			while l ~= nil do
+				l, err, rBuf = socket:receive("*l", rBuf)
+				if not l then
+						if err ~= "timeout" then
+							Network.CloseConnection()
+							Isaac.DebugString("Read error:"..err)
+						end
+				else
+						--Isaac.DebugString(l)
+						lastLine = l
+				end
+			end
+			return lastLine
+	end
+
+	function Network.SendData(data)
+			if currentBehaviour == Behaviour.CLIENT then
+					client:send(data.."\n")
+			end
+	end
+
+	function Network.StartClient()
+			if currentBehaviour == Behaviour.IDLE then
+					client = assert(socket.tcp())
+					client:connect(connectIP,currentPort)
+					currentBehaviour = Behaviour.CLIENT
+					client:settimeout(loopTimeout) -- set the connection timeout
+
+					local CID,err,prt = client:receive()
+					if err~="timeout" and err~=nil then
+						Network.CloseConnection();
+						Isaac.DebugString("CID error:"..err);
+					elseif err=="timeout" then
+						Network.CloseConnection();
+						Isaac.DebugString("timeout at CID :"..(CID or "")..(prt or ""));
+					elseif err==nil then
+						seed = CID:sub(1,9)
+						ClID = CID:sub(10)
+						Isaac.DebugString("CID : "..ClID);
+						Isaac.DebugString("Seed : "..seed);
+						if IsaacNetwork.useServerSeed then
+							Isaac.ExecuteCommand("seed "..seed)
+							baseSeed = seed;
+							IsaacNetwork.baseSeed = baseSeed;
+						end
+						clientID = tonumber(ClID)
+						IsaacNetwork.clientID = clientID
+					end
+			end
+	end
+
+	function Network.CloseConnection()
+			if currentBehaviour ~= Behaviour.IDLE then
+					if currentBehaviour == Behaviour.CLIENT then
+							Network.SendData("[END]"..clientID)
+							client:close()
+							clientID = 0
+							IsaacNetwork.clientID = clientID
+							connectedClients = {}
+							IsaacNetwork.connectedClients = connectedClients;
+					end
+					currentBehaviour = Behaviour.IDLE
+			end
+	end
+	IsaacNetwork.CloseConnection = function() Network.CloseConnection() end;
+
+function Mod:RenderLoop()
+		ClientThread()
+end
+
+local function getArgs(params)
+	args={}
+	for i in string.gmatch(params, "%S+") do
+	 args[#args+1] = i
+	end
+	return args
+end
+
+function Mod:Command(command, params)
+	args = getArgs(params)
+	if clientID == 0 and command == "connect" then
+		if #args >= 1 then
+			connectIP = args[1];
+			IsaacNetwork.connectIP = connectIP
+			Network.StartClient()
+		end
+	end
+	if command == "disconnect" then
+		Network.CloseConnection()
+	end
+	if clientID == 0 and command == "pluginlist" then
+		Isaac.ConsoleOutput( "IsaacNetwork : " )
+		for k,v in pairs(Plugins.Names) do
+			Isaac.ConsoleOutput("	- " .. v)
+		end
+	end
+end
+
+function Mod:CloseConnection(shouldSave)
+	if not shouldSave then
+--		Network.CloseConnection();
+	end
+end
+
+Mod:AddCallback(ModCallbacks.MC_POST_RENDER, Mod.RenderLoop)
+Mod:AddCallback(ModCallbacks.MC_EXECUTE_CMD, Mod.Command)
+--Mod:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, Mod.CloseConnection)
+
+-- PLUGINS --
+
+local function BasicInfoBuilder()
+	local player = Isaac.GetPlayer(0);
+	local table = {}
+	table.Seed = game:GetSeeds():GetStartSeed();
+	table.Level = game:GetLevel():GetAbsoluteStage();
+	table.Room = game:GetLevel():GetCurrentRoomIndex();
+	table.PositionX = player.Position.X;
+	table.PositionY = player.Position.Y;
+	table.Name = player:GetName();
+	return table
+end
+
+local function BasicInfoProcessor(table)
+
+end
+
+IsaacNetwork.RegisterNetworkPlugin("BasicInfo", BasicInfoBuilder, BasicInfoProcessor)
